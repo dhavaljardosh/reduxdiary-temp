499c0f6a13d71c0cff94c9ae8103f0671ff4af9b
Dhaval Jardosh <dhavaljardosh@csu.fullerton.edu>
2020-11-02 02:48:51 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { store } from "./index";
 
 function App() {
+  const [contactName, setContactName] = useState("");
+  const [note, setNote] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ padding: "10px 80px 0px" }}>
+        <div style={{ display: "flex" }}>
+          <div className="box">
+            <h1>Contacts</h1>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                if (!contactName) {
+                  alert("Contact is required");
+                  return;
+                }
+                store.dispatch({ type: "ADD_CONTACT", name: contactName });
+              }}
+            >
+              <input
+                className="btn-input"
+                onChange={(e) => setContactName(e.target.value)}
+              />
+              <button className="bal-btn" type="submit">
+                Add
+              </button>
+            </form>
+
+            <div>
+              {store.getState().contactsReducer.map((contact) => (
+                <Contact details={contact} />
+              ))}
+            </div>
+          </div>
+          <div className="box">
+            <h1>Notes</h1>
+            <input
+              className="btn-input"
+              onChange={(e) => setNote(e.target.value)}
+            />
+            <button
+              className="bal-btn"
+              onClick={() => {
+                if (!note) {
+                  alert("Contact is required");
+                  return;
+                }
+                store.dispatch({ type: "ADD_NOTE", noteDescription: note });
+              }}
+            >
+              Add
+            </button>
+            <div>
+              {store.getState().notesReducer.map((note) => (
+                <Note details={note} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Contact({ details }) {
+  return (
+    <div className="contact-detail">
+      <div className="contact-name">{details.name}</div>
+      <div>
+        <div className="delete-btn">Delete</div>
+      </div>
+    </div>
+  );
+}
+
+function Note({ details }) {
+  return (
+    <div className="contact-detail">
+      <div className="contact-name">{details.noteDescription}</div>
+      <div>
+        <div className="delete-btn">Delete</div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { combineReducers, createStore } from "redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const contactsReducer = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_CONTACT": {
+      let newState = [...state, { name: action.name }];
+      return newState;
+    }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    case "DELETE_CONTACT": {
+      let newState = [...state];
+      newState.pop();
+      return newState;
+    }
+
+    default:
+      console.log("UNKNOWN");
+      return state;
+  }
+};
+
+const notesReducer = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_NOTE": {
+      let newState = [...state, { noteDescription: action.noteDescription }];
+      return newState;
+    }
+
+    case "DEPOSIT_MONEY": {
+      let newState = [...state, { name: action.name }];
+      return newState;
+    }
+
+    default:
+      console.log("UNKNOWN");
+      return state;
+  }
+};
+
+const reducer = combineReducers({ contactsReducer, notesReducer });
+
+export const store = createStore(reducer, {});
+console.log(store.getState());
+const render = () => {
+  return ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+};
+render();
+store.subscribe(render);
